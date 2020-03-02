6752ee113df5f14e49fd4de2530f958c89bb7eee
whuaz <18610941573@163.com>
2020-03-02 19:04:14 +0800

    first commit

---
+++ src/main/java/com/whuaz/WorkSheetApplication.java
@@ -0,0 +1,13 @@
+package com.whuaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkSheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkSheetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whuaz/WorkSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whuaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkSheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
